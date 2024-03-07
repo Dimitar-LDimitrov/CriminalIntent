0506106f3ed3074b958a2840c6a8b24268421ae5
Dimitar Dimitrov - SD <dimitar.ly.dimitrov@vivacom.bg>
2024-03-07 15:46:03 +0200

    fragment and layout for Crime listing created

---
+++ app/src/main/java/com/dimitrov/criminalintent/fragments/CrimeListFragment.kt
@@ -0,0 +1,48 @@
+package com.dimitrov.criminalintent.fragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.dimitrov.criminalintent.CrimeListViewModel
+import com.dimitrov.criminalintent.databinding.FragmentCrimeListBinding
+
+private const val TAG = "CrimeListFragment"
+class CrimeListFragment : Fragment() {
+    // bind the layout
+    private var _binding: FragmentCrimeListBinding? = null
+    private val binding
+        get() = checkNotNull(_binding) {
+            "Cannot access binding because it is null. Is the view visible?"
+        }
+
+
+    private val crimeListViewModel: CrimeListViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "Total crimes: ${crimeListViewModel.crimes.size}")
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCrimeListBinding.inflate(inflater, container, false)
+
+        binding.crimeListRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        return binding.root
+    }
+
+    // null out the binding
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
