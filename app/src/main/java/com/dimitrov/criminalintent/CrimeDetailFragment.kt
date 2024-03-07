8817bc324b288fd74aca08b7003db74fb018fa95
Dimitar Dimitrov - SD <dimitar.ly.dimitrov@vivacom.bg>
2024-03-07 09:56:20 +0200

    Crime model created and Crime Detail fragment

---
+++ app/src/main/java/com/dimitrov/criminalintent/CrimeDetailFragment.kt
@@ -0,0 +1,61 @@
+package com.dimitrov.criminalintent
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
+import com.dimitrov.criminalintent.databinding.FragmentCrimeDetailBinding
+import java.util.Date
+import java.util.UUID
+
+class CrimeDetailFragment : Fragment() {
+
+    private lateinit var crime: Crime
+    private lateinit var binding: FragmentCrimeDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        crime = Crime(
+            id = UUID.randomUUID(),
+            title = "",
+            date = Date(),
+            isSolved = false
+        )
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCrimeDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        /* Here, you add a listener that will be invoked whenever the text in the EditText is changed.
+        The lambda is invoked with four parameters, but you only care about the first one, text.
+        The text is provided as a CharSequence, so to set the Crime’s title, you call toString() on it. */
+        binding.apply {
+
+            etTitle.doOnTextChanged { text, _, _, _ ->
+                crime = crime.copy(title = text.toString())
+            }
+
+            btnDateTime.apply {
+                text = crime.date.toString()
+                isEnabled = false
+            }
+
+            // Set a listener on the CheckBox that will update the isSolved property of the Crime
+            cbIsSolved.setOnCheckedChangeListener { _, isChecked ->
+                crime = crime.copy(isSolved = isChecked)
+            }
+        }
+    }
+}
